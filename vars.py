3a98654142d6e99c72d620af6e1ded77c5febfac
GajendraSingh2000 <gajurpg@gmail.com>
2025-06-02 19:45:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23111859"))
 API_HASH = environ.get("API_HASH", "e83a8fc0c228487183f74de936eafd21")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7502173366:AAHwD8NnT8vCP3oUDwunCCbMXkmw6fTiISc")
 OWNER = int(environ.get("OWNER", "7541970367"))
 CREDIT = "𝄟⃝‌🐬Gajendra Singh🐬"
 #WEBHOOK = True  # Don't change this
